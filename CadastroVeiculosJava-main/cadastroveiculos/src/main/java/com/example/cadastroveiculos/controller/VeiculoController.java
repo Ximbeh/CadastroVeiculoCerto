2e899cdfb8608182324dd733f1093cf5010d52d9
Ximb <100860445+Ximbeh@users.noreply.github.com>
2023-08-24 12:43:17 -0300

    Add files via upload

--- CadastroVeiculosJava-main/cadastroveiculos/src/main/java/com/example/cadastroveiculos/controller/VeiculoController.java
+++ CadastroVeiculosJava-main/cadastroveiculos/src/main/java/com/example/cadastroveiculos/controller/VeiculoController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-@CrossOrigin(origins = "http://localhost:5173")//Endereço do front
+@CrossOrigin(origins = "http://127.0.0.1:5173")//Endereço do front
 @RestController
 @RequestMapping("/veiculos")
 public class VeiculoController {
